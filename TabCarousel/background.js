37cb478f72a1123288af30917fdaca01c4a17d56
João V. M. <99231431+JoaoMCV@users.noreply.github.com>
2024-07-28 19:37:14 -0300

    Add files via upload

---
+++ TabCarousel/background.js
@@ -0,0 +1,87 @@
+let index = 0
+let estado = false
+let intervalo = 5000
+let intervaloId = null
+let devagar = 10000
+let padrao = 5000
+let rapido = 2000
+let removedTabs
+
+function updateInterval(novoIntervalo){
+    if(novoIntervalo >0){
+        intervalo = novoIntervalo
+    }
+}
+
+function updateEstado(novoEstado){
+    if(typeof novoEstado === Boolean){
+        estado = novoEstado
+    }
+}
+function inverteEstado(){
+    estado = !estado
+}
+
+function iniciaPara(novoIntervalo){
+    intervalo= novoIntervalo
+    clearInterval(intervaloId)
+    console.log('troca de intervalo')
+    if(estado){
+        intervaloId = setInterval(carossel, intervalo)
+    }
+}
+
+function carossel(){
+    if(!estado) return
+    chrome.tabs.query({}, (tabs)=>{
+        if(tabs.length === 0) return
+        console.log(tabs[index].id)
+        teste = chrome.tabs.getCurrent()
+        console.log(teste.index)
+        index = (index + 1) % tabs.length
+        chrome.tabs.update(tabs[index].id, {active: true})
+    })
+    
+}
+
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse)=>{
+    
+    if(request.action === 'start/stop'){
+        inverteEstado()
+        iniciaPara(intervalo)
+        sendResponse({estado})
+    }
+    else if(request.action === 'getEstado'){
+        sendResponse({estado})
+    }
+    else if(request.action === 'getIntervalo'){
+        sendResponse({intervalo})
+    }
+    else if(request.action === 'getAll'){
+        sendResponse({estado, intervalo})
+    }
+    else if(request.action === 'setEstado'){
+        inverteEstado()
+        sendResponse(estado ? 'mudado para ligado' : 'mudado para desligado')
+    }
+    else if(request.action === 'setVelocidadeDevagar'){
+        iniciaPara(devagar)
+        sendResponse('trocou')
+    }
+    else if(request.action === 'setVelocidadePadrao'){
+        iniciaPara(padrao)
+        sendResponse('trocou')
+    }
+    else if(request.action === 'setVelocidadeRapida'){
+        iniciaPara(rapido)
+        sendResponse('trocou')
+    }
+    else if(request.action === 'setVelocidadeCustom'){
+        intervalo = request.message 
+        iniciaPara(intervalo)
+        sendResponse('trocou')
+    }
+
+    return
+})
--- TabCarousel/popup-script.js
+++ TabCarousel/popup-script.js
@@ -1,6 +1,51 @@
-let cycling = true;
-    document.getElementById('toggle').addEventListener('click', () => {
-      cycling = !cycling;
-      chrome.runtime.sendMessage({ cycling });
-      document.getElementById('toggle').textContent = cycling ? 'Stop Cycling' : 'Start Cycling';
-    });
+botao = document.getElementById('startstop');
+speedSelectDevagar = document.getElementById('devagar')
+speedSelectRapido = document.getElementById('rapido')
+speedSelectPadrao = document.getElementById('padrao')
+input = document.getElementById('custom');
+inputSetBtn = document.getElementById('customset');
+
+
+function updateButton(estadoAtual){
+    
+    if(estadoAtual){
+        botao.textContent = 'Parar'
+        botao.classList.add('ligado')
+        botao.classList.remove('desligado')
+    }
+    else{
+        botao.textContent = 'Começar'
+        botao.classList.add('desligado')
+        botao.classList.remove('ligado')        
+    }
+}
+
+inputSetBtn.addEventListener('click', ()=>{
+    chrome.runtime.sendMessage({action: 'setVelocidadeCustom', message: input.value}, ()=>{
+        console.log('Intervalo alterado para' + input.value + 'segundos')
+    })
+})
+
+botao.addEventListener('click', ()=>{
+    chrome.runtime.sendMessage({action: 'start/stop'},(response)=>{
+        console.log(response.estado)
+        updateButton(response.estado)
+    })
+})
+
+speedSelectDevagar.addEventListener('click', ()=>{
+    chrome.runtime.sendMessage({action: 'setVelocidadeDevagar'}, ()=>{
+        console.log('Intervalo alterado para devagar (10s)')
+    })
+})
+
+speedSelectPadrao.addEventListener('click', ()=>{
+    chrome.runtime.sendMessage({action: 'setVelocidadePadrao'}, ()=>{
+        console.log('Intervalo alterado para Padrao (5s)')
+    })
+})
+speedSelectRapido.addEventListener('click', ()=>{
+    chrome.runtime.sendMessage({action: 'setVelocidadeRapida'}, ()=>{
+        console.log('Intervalo alterado para Padrao (2s)')
+    })
+})
